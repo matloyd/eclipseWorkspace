826b72c4d236f97f8c6b99ccdedb5ec48de578bf
rosen <matloyd17@gmail.com>
2018-06-14 15:28:53 +0430

    app commited and pushed to github

---
+++ demo/src/main/java/com/mohsen/jpa/hibernate/demo/DemoApplication.java
@@ -0,0 +1,12 @@
+package com.mohsen.jpa.hibernate.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoApplication.class, args);
+	}
+}
