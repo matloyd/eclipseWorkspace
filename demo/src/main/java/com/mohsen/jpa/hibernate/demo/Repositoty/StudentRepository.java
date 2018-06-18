70019fd2f2b0b1333f17d1bc0cd56131e346da04
Mohsen R Aqdam <matloyd17@gmail.com>
2018-06-18 17:50:43 +0430

    StudentRepository added

---
+++ demo/src/main/java/com/mohsen/jpa/hibernate/demo/Repositoty/StudentRepository.java
@@ -0,0 +1,46 @@
+package com.mohsen.jpa.hibernate.demo.Repositoty;
+
+import javax.persistence.EntityManager;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.mohsen.jpa.hibernate.demo.entity.Passport;
+import com.mohsen.jpa.hibernate.demo.entity.Student;
+
+@Repository
+@Transactional
+public class StudentRepository {
+
+	@Autowired
+	EntityManager em;
+	private Logger logger=LoggerFactory.getLogger(this.getClass());
+
+	public Student findById(Long id) {
+		return em.find(Student.class,id);
+	}
+	
+	public Student save(Student student) {
+		if(student.getId()==null)
+			em.persist(student);
+		else
+			em.merge(student);
+		return student;
+	}
+	
+	public void deleteById(Long id) {
+		em.remove(findById(id));
+	}
+	
+	public void saveStudentWithPassport() {
+		Passport p1=new Passport("Q74482");
+		Student s1=new Student("Mike");
+		
+		em.persist(s1);
+		
+	}
+
+}
--- demo/src/main/resources/data.sql
+++ demo/src/main/resources/data.sql
@@ -7,13 +7,13 @@ values (10003,'SQL Methods',sysdate(),sysdate());
 insert into course(id,name,created_date,last_updated_date) 
 values (10004,'Hibernate Master Class',sysdate(),sysdate());
 
-insert into student(id,name) values (20001,'Ranga');
-insert into student(id,name) values (20002,'Adam');
-insert into student(id,name) values (20003,'Jane');
-
 insert into passport(id,number) values (40001,'R55849');
 insert into passport(id,number) values (40002,'A09890');
 insert into passport(id,number) values (40003,'J20458');
+
+insert into student(id,name,PASSPORT_ID) values (20001,'Ranga',40001);
+insert into student(id,name,PASSPORT_ID) values (20002,'Adam',40002);
+insert into student(id,name,PASSPORT_ID) values (20003,'Jane',40003);
 
 insert into review(id,rating,description) values (50001,'5','Great course');
 insert into review(id,rating,description) values (50002,'2','Bad coverage');
